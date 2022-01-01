3a61e1cee34ab4fe4327112b83c57fc7f4723cdb
fyfq <iamtomblack@outlook.com>
2021-12-31 20:13:39 -0800

    完善部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-yum install git
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
+echo '--------GIT及CURL--------'
+cd ~
+sudo apt install -y git
+sudo apt install -y curl
+echo '--------克隆HEXO--------'
+git clone https://hub.fastgit.org/fyfq/fyfq.github.io.git
+cd fyfq.github.io
+echo '--------安装NVM--------'
+curl -o- https://raw.fastgit.org/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
-
+echo '--------安装NPM--------'
+nvm install 16
 npm config set registry http://mirrors.cloud.tencent.com/npm/
 npm config get registry
-nvm install 16
+echo '--------安装HEXO--------'
 npm install -g hexo-cli
 npm install hexo-deployer-git --save
 npm install hexo-generator-searchdb --save
-git config --global user.mail "iamtomblack@outlook.com"
-git config --global user.user "fyfq"
+echo '--------配置GITHUB--------'
+git config --global user.email "iamtomblack@outlook.com"
+git config --global user.name "fyfq"
 ssh-keygen -t ed25519 -C "iamtomblack@outlook.com"
 echo '粘贴公钥到github后使用 git -T git@github.com 测试连通性'
